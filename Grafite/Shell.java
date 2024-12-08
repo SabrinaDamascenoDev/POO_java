5648d2aed15187665c3f2eaa113466e78d07507e
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-12-07 21:21:52 -0300

    Questão do grafite e lapiseira

---
+++ Grafite/Shell.java
@@ -0,0 +1,194 @@
+import java.util.Scanner;
+class Lead {
+    private int size;
+    private String hardness;
+    private double thickness;
+    private Pencil uma;
+
+    public Lead(double thickness, String hardness, int size){
+        this.size = size;
+        this.hardness = hardness;
+        this.thickness = thickness;
+    }
+
+    public int usagePerSheet(){
+        if(hardness.equals("HB")){
+            return 1;
+        }
+        if(hardness.equals("2B")){
+            return 2;
+        }
+        if(hardness.equals("4B")){
+            return 4;
+        }
+        if(hardness.equals("6B")){
+            return 6;
+        } else {
+            return 0;
+        }
+
+    }
+
+    public double getThickness(){
+        return this.thickness;
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public String getHardness(){
+        return this.hardness;
+    }
+
+    public void setSize(int valor){
+        this.size = valor;
+    }
+
+    public String toString() {
+        return String.format("[%.1f:%s:%d]", this.thickness, this.hardness, this.size);
+    }
+
+}
+class Pencil {
+    private Lead tip;
+    private double thickness;
+
+    public Pencil(double thickness){
+        this.thickness = thickness;
+        this.tip = null;
+    }
+
+    public boolean hasGrafite() {
+        if(tip != null){
+            return true;
+        } else {
+            System.out.println("fail: nao existe grafite");
+            return false;
+        }
+    }
+
+    public boolean insert(Lead lead){
+
+        if(tip != null){
+            System.out.println("fail: ja existe grafite");
+            return false;
+        }  else {
+            this.tip = lead;
+            if(tip.getThickness() != this.thickness){
+                System.out.println("fail: calibre incompativel");
+                this.tip = null;
+            }
+            return true;
+        }
+    }
+
+    public Lead remove(){
+        if(this.tip == null){
+            System.out.println("fail: nao existe grafite");
+            return null;
+        } else {
+
+            tip = null;
+            return tip;
+        }
+    }
+
+    public void writePage(){
+        if(this.tip == null){
+            System.out.println("fail: nao existe grafite");
+        } else if((tip.getSize()-tip.usagePerSheet()/2) < 10){
+            System.out.println("fail: tamanho insuficiente");
+        } else {
+            if(tip.usagePerSheet() == 1){
+                if((tip.getSize()-1) < 10){
+                    System.out.println("fail: folha incompleta");
+                    tip.setSize(10);
+                } else {
+                    tip.setSize(tip.getSize()-1);
+                }
+            } else if(tip.usagePerSheet() == 2){
+                if((tip.getSize()-2) < 10){
+                    System.out.println("fail: folha incompleta");
+                    tip.setSize(10);
+                } else {
+                    tip.setSize(tip.getSize()-2);
+                }
+            } else if(tip.usagePerSheet() == 4){
+                if((tip.getSize()-4) < 10){
+                    System.out.println("fail: folha incompleta");
+                    tip.setSize(10);
+                    return;
+                } else {
+                    int valor4 = tip.getSize();
+                    tip.setSize(valor4-4);
+                }
+            } else if(tip.usagePerSheet() == 6){
+                if((tip.getSize()-6) < 10){
+                    System.out.println("fail: folha incompleta");
+                    tip.setSize(10);
+                } else {
+                    int valor = tip.getSize();
+                    tip.setSize(valor-6);
+                }
+            }
+        }
+    }
+
+    public String toString(){
+        if(tip == null){
+            return String.format("calibre: %.1f, grafite: null", thickness);
+        } else {
+            return String.format("calibre: %.1f, grafite: %s", thickness, tip.toString());
+        }
+
+    }
+
+}
+class Adapter {
+    private Pencil pencil;
+    public Adapter(double thickness) {
+        pencil = new Pencil(thickness);
+    }
+
+    public void insert(double thickness, String hardness, int length) {
+        pencil.insert(new Lead(thickness, hardness, length));
+    }
+
+    public void remove() {
+        pencil.remove();
+    }
+
+    public void writePage() {
+        pencil.writePage();
+    }
+
+    public void show() {
+        System.out.println(pencil.toString());
+    }
+}
+
+
+public class Shell {
+    public static void main(String[] _args) {
+        Adapter adp = new Adapter(0.5f);
+
+        while (true) {
+            String line = input();
+            String[] args = line.split(" ");
+            write('$' + line);
+
+            if      ("end".equals(args[0])   ) { break;                                                       }
+            else if ("init".equals(args[0])  ) { adp = new Adapter(number(args[1]));                          }
+            else if ("insert".equals(args[0])) { adp.insert(number(args[1]), args[2], (int) number(args[3])); }
+            else if ("remove".equals(args[0])) { adp.remove();                                                }
+            else if ("write".equals(args[0]) ) { adp.writePage();                                             }
+            else if ("show".equals(args[0])  ) { adp.show();                                                  }
+        }
+    }
+
+    static Scanner scanner = new Scanner(System.in);
+    public static String input()           { return scanner.nextLine();    }
+    public static void write(String value) { System.out.println(value);    }
+    public static double number(String str) { return Double.parseDouble(str); }
+}
