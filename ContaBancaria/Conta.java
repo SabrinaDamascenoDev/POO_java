b1b840ff6d3a569577ee5a47c8b7b8d75c06d6e0
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-12-10 18:23:28 -0300

    Ajustes

--- ContaBancaria/Conta.java
+++ ContaBancaria/Conta.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 class Conta {
-    private String titular;
-    private int numero;
+    private final String titular;
+    private final int numero;
     private double saldo;
     private static int totalContas = 0;
 
@@ -25,14 +25,12 @@ class Conta {
         c.deposita(valor);
     }
 
-    boolean saca(double valor) {
+    void saca(double valor) {
         if ((this.saldo - valor) < 0) {
-            System.out.println(String.format("Conta ficou zerada após o saque %.2f reais", this.saldo));
+            System.out.printf("Conta ficou zerada após o saque %.2f reais%n", this.saldo);
             this.saldo = 0;
-            return false;
         } else {
             this.saldo -= valor;
-            return true;
         }
     }
 
@@ -84,33 +82,44 @@ class Solver{
 
         ArrayList<Conta> contas = new ArrayList<Conta>();
 
+        label:
         while(true) {
             String line = scanner.nextLine();
             String ui[] = line.split(" ");
             System.out.println("$" + line);
 
-            if(ui[0].equals("init")) {
-                contas.add( new Conta( ui[1] ) );
-            } else if(ui[0].equals("end")) {
-                break;
-            } else if(ui[0].equals("deposita")) {
-                int nro = Integer.parseInt(ui[1] );
-                double valor = Double.parseDouble(ui[2]);
-                Conta.deposita( contas.get(nro),  valor);
-            }else if(ui[0].equals("saca")) {
-                int nro = Integer.parseInt(ui[1] );
-                double valor = Double.parseDouble(ui[2]);
-                Conta.saca( contas.get(nro),  valor);
-            }else if(ui[0].equals("transfere")) {
-                int origem = Integer.parseInt(ui[1] );
-                int destino = Integer.parseInt(ui[2] );
-                double valor = Double.parseDouble(ui[3]);
-                Conta.transfere( contas.get(origem), contas.get(destino), valor);
-            } else if(ui[0].equals("show")) {
-                int i = Integer.parseInt(ui[1] );
-                System.out.println( contas.get(i) );
-            } else{
-                System.out.println("fail: comando invalido");
+            switch (ui[0]) {
+                case "init":
+                    contas.add(new Conta(ui[1]));
+                    break;
+                case "end":
+                    break label;
+                case "deposita": {
+                    int nro = Integer.parseInt(ui[1]);
+                    double valor = Double.parseDouble(ui[2]);
+                    Conta.deposita(contas.get(nro), valor);
+                    break;
+                }
+                case "saca": {
+                    int nro = Integer.parseInt(ui[1]);
+                    double valor = Double.parseDouble(ui[2]);
+                    Conta.saca(contas.get(nro), valor);
+                    break;
+                }
+                case "transfere": {
+                    int origem = Integer.parseInt(ui[1]);
+                    int destino = Integer.parseInt(ui[2]);
+                    double valor = Double.parseDouble(ui[3]);
+                    Conta.transfere(contas.get(origem), contas.get(destino), valor);
+                    break;
+                }
+                case "show":
+                    int i = Integer.parseInt(ui[1]);
+                    System.out.println(contas.get(i));
+                    break;
+                default:
+                    System.out.println("fail: comando invalido");
+                    break;
             }
         }
         scanner.close();
