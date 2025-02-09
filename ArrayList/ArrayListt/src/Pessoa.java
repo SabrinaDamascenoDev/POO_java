1687bd83e72a62c6ab7f8d7f3bedbf1a77a9b1cd
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2025-02-09 19:21:15 -0300

    Usando ArrayLists

--- ArrayList/ArrayListt/src/Pessoa.java
+++ ArrayList/ArrayListt/src/Pessoa.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Pessoa {
+    private String nome;
+    private ArrayList<Telefone> telefones;
+
+    public Pessoa(String nome){
+        this.nome = nome;
+        this.telefones = new ArrayList<Telefone>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setTelefones(ArrayList<Telefone> telefones){
+        this.telefones = telefones;
+    }
+     public void addTelefone(Telefone tel){
+        this.telefones.add(tel);
+     }
+
+     public void removerTelefone(Telefone tel){
+        this.telefones.remove(tel);
+     }
+     public void listarNumeros() {
+        for(Telefone tel : telefones){
+            System.out.println(tel.getNumero());
+        }
+     }
 }
--- ArrayList/ArrayListt/src/Telefone.java
+++ ArrayList/ArrayListt/src/Telefone.java
@@ -1,2 +1,30 @@
 public class Telefone {
+    private String tipo;
+    private  String ddd;
+    private String numero;
+    public Telefone(String tipo, String ddd, String numero){
+        this.ddd = ddd;
+        this.numero = numero;
+        this.tipo = tipo;
+    }
+    public String getTipo(){
+        return this.tipo;
+    }
+    public String getDdd(){
+        return this.ddd;
+    }
+    public String getNumero(){
+        return this.numero;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+    public void setDdd(String ddd){
+        this.ddd = ddd;
+    }
+    public void setNumero(String numero){
+        this.numero = numero;
+    }
 }
+
